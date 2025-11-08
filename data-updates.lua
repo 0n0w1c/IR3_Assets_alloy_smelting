4f18e7f0bf63f60ae687cbb3f079eeedc9c18306
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2025-11-08 10:53:47 -0600

    Version 0.0.4

--- data-updates.lua
+++ data-updates.lua
@@ -6,13 +6,33 @@ if not (mods["IndustrialRevolution3Assets1"]
     return
 end
 
+local recipe
+
 require("prototypes/explosion/brick-kiln")
 require("prototypes/entity/brick-kiln")
 require("prototypes/item/brick-kiln")
 require("prototypes/technology/kiln-smelting")
 
+recipe = data.raw["recipe"]["brick-kiln"]
+if mods["quality"] and recipe then
+    local recycling = require("__quality__/prototypes/recycling")
+    recycling.generate_recycling_recipe(recipe)
+end
+
 require("prototypes/explosion/electric-kiln")
 require("prototypes/entity/electric-kiln")
 require("prototypes/item/electric-kiln")
 
+recipe = data.raw["recipe"]["electric-kiln"]
+if mods["quality"] and recipe then
+    local recycling = require("__quality__/prototypes/recycling")
+    recycling.generate_recycling_recipe(recipe)
+end
+
 require("prototypes/item/coke")
+
+recipe = data.raw["recipe"]["coke"]
+if mods["quality"] and recipe then
+    local recycling = require("__quality__/prototypes/recycling")
+    recycling.generate_recycling_recipe(recipe)
+end
